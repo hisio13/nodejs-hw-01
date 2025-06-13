a22d6ce12db95375f08a44437f707587a95770ef
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-06-13 11:50:24 +0200

    update config

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -2,10 +2,12 @@ import { createFakeContact } from '../utils/createFakeContact.js';
 import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
-const generateContacts = async (number) => {
-    const contacts = await readContacts();
-    const newContacts = Array.from({ length: count }, createFakeContact);
-    const updatedContacts = [...contacts, ...newContacts];
-    await writeContacts(updatedContacts);};
+const generateContacts = async (count = 5) => {
+  const contacts = await readContacts();
+  const newContacts = Array.from({ length: count }, createFakeContact);
+  const updatedContacts = [...contacts, ...newContacts];
+  await writeContacts(updatedContacts);
+};
 
 generateContacts(5);
+
